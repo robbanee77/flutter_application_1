ec0614b93195915a7ffb420eed9bcb0abebbb27f
robbanee77 <robbanaa4959@gmail.com>
2023-07-25 21:47:26 +0700

    new

--- lib/screen/request.dart
+++ lib/screen/request.dart
@@ -18,7 +18,7 @@ class _requestState extends State<request> {
       backgroundColor: Color(0xFFe6ebe0),
       appBar: AppBar(
         title: Row(
-          children: [SizedBox(width: 8), Text('Course resevation')],
+          children: [SizedBox(width: 8), Text('Course reservation')],
         ),
         backgroundColor: Color(0xFF5ca4a9),
         actions: [
@@ -34,8 +34,6 @@ class _requestState extends State<request> {
                 context,
                 MaterialPageRoute(builder: (context) => notioff()),
               );
-              // ดักเหตุการณ์เมื่อกดปุ่ม Info ที่อยู่ฝั่งซ้ายของ AppBar
-              // คุณสามารถใส่โค้ดที่คุณต้องการทำเมื่อกดปุ่มนี้ได้ที่นี่
             },
           ),
           IconButton(
@@ -50,61 +48,117 @@ class _requestState extends State<request> {
                 context,
                 MaterialPageRoute(builder: (context) => histoty()),
               );
-              // ดักเหตุการณ์เมื่อกดปุ่ม Info ที่อยู่ฝั่งซ้ายของ AppBar
-              // คุณสามารถใส่โค้ดที่คุณต้องการทำเมื่อกดปุ่มนี้ได้ที่นี่
             },
           ),
         ],
       ),
       body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: Color(0xFF5ca4a9),
-            border:
-                Border.all(color: Color.fromARGB(255, 255, 255, 255), width: 2),
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Text(
-              //  'กรอกข้อมูลที่ต้องการ',
-              // style: TextStyle(
-              //    fontSize: 18,
-              //   color: Colors.white), // เปลี่ยนสีของตัวอักษรเป็นสีขาว
-              // ),
-              SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 6,
-                  itemBuilder: (context, index) {
-                    final data = [
-                      '621431010',
-                      'Robbanee Beraheng',
-                      'Faculty of Education',
-                      'English language teaching',
-                      'Freedom subject',
-                      'Group 01',
-                    ];
-
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Text(
-                        '${index + 1}. ${data[index]}',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color:
-                                Colors.white), // เปลี่ยนสีของตัวอักษรเป็นสีขาว
-                      ),
-                    );
-                  },
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Color(0xFF5ca4a9),
+                border: Border.all(
+                    color: Color.fromARGB(255, 255, 255, 255), width: 2),
+                borderRadius: BorderRadius.circular(25),
               ),
-            ],
-          ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Your existing ListView.builder goes here
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        final data = [
+                          '621431010',
+                          'Robbanee Beraheng',
+                          'Faculty of Education',
+                          'English language teaching',
+                          'Freedom subject',
+                          'Group 01',
+                        ];
+
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            '${index + 1}. ${data[index]}',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+                height: 10), // Add some spacing between the text and buttons
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle the "ยืนยัน" button press
+                    },
+                    child: Text('ยืนยัน'),
+                  ),
+                  SizedBox(width: 10), // Add spacing between the buttons
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle the "ยกเลิก" button press
+                    },
+                    child: Text('ยกเลิก'),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Color(0xFF5ca4a9),
+                border: Border.all(
+                    color: Color.fromARGB(255, 255, 255, 255), width: 2),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Your existing ListView.builder goes here
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        final data = [
+                          '621431010',
+                          'Robbanee Beraheng',
+                          'Faculty of Education',
+                          'English language teaching',
+                          'Freedom subject',
+                          'Group 01',
+                        ];
+
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            '${index + 1}. ${data[index]}',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
