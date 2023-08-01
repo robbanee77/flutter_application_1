7ee093ca3a773c6645ea762fe916bdd1e6a666c6
robbanee77 <robbanaa4959@gmail.com>
2023-08-01 14:16:31 +0700

    new

--- lib/screen/request.dart
+++ lib/screen/request.dart
@@ -75,12 +75,12 @@ class _requestState extends State<request> {
                           itemCount: 6,
                           itemBuilder: (context, index) {
                             final data = [
-                              '621431010',
-                              'Robbanee Beraheng',
+                              '611431022',
+                              'Roiyan Umudee',
                               'Faculty of Education',
                               'English language teaching',
                               'Freedom subject',
-                              'Group 01',
+                              'Group 02',
                             ];
 
                             return Padding(
@@ -107,14 +107,20 @@ class _requestState extends State<request> {
                       onPressed: () {
                         // Handle the "ยืนยัน" button press
                       },
-                      child: Text('ยืนยัน'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF5ca4a9), // Set button color here
+                      ),
+                      child: Text('Confirm'),
                     ),
                     SizedBox(width: 10), // Add spacing between the buttons
                     ElevatedButton(
                       onPressed: () {
                         // Handle the "ยกเลิก" button press
                       },
-                      child: Text('ยกเลิก'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFed6a5a), // Set button color here
+                      ),
+                      child: Text('Cancel'),
                     ),
                   ],
                 ),
@@ -172,14 +178,162 @@ class _requestState extends State<request> {
                       onPressed: () {
                         // Handle the "ยืนยัน" button press
                       },
-                      child: Text('ยืนยัน'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF5ca4a9), // Set button color here
+                      ),
+                      child: Text('Confirm'),
                     ),
                     SizedBox(width: 10), // Add spacing between the buttons
                     ElevatedButton(
                       onPressed: () {
                         // Handle the "ยกเลิก" button press
                       },
-                      child: Text('ยกเลิก'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFed6a5a), // Set button color here
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ), // Add some spacing between the buttons and the second container
+                Container(
+                  width: 300,
+                  height: 300,
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF5ca4a9),
+                    border: Border.all(
+                        color: Color.fromARGB(255, 255, 255, 255), width: 2),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Your existing ListView.builder goes here
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: 6,
+                          itemBuilder: (context, index) {
+                            final data = [
+                              '621431022',
+                              'Sulaiman hama',
+                              'Faculty of Education',
+                              'English language teaching',
+                              'Freedom subject',
+                              'Group 01',
+                            ];
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: Text(
+                                '${index + 1}. ${data[index]}',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ), // Add some spacing below the second container
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the "ยืนยัน" button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF5ca4a9), // Set button color here
+                      ),
+                      child: Text('Confirm'),
+                    ),
+                    SizedBox(width: 10), // Add spacing between the buttons
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the "ยกเลิก" button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFed6a5a), // Set button color here
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ), // Add some spacing between the buttons and the second container
+                Container(
+                  width: 300,
+                  height: 300,
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF5ca4a9),
+                    border: Border.all(
+                        color: Color.fromARGB(255, 255, 255, 255), width: 2),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Your existing ListView.builder goes here
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: 6,
+                          itemBuilder: (context, index) {
+                            final data = [
+                              '621431023',
+                              'Farhan Umudee',
+                              'Faculty of Education',
+                              'English language teaching',
+                              'Freedom subject',
+                              'Group 01',
+                            ];
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: Text(
+                                '${index + 1}. ${data[index]}',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ), // Add some spacing below the second container
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the "ยืนยัน" button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF5ca4a9), // Set button color here
+                      ),
+                      child: Text('Confirm'),
+                    ),
+                    SizedBox(width: 10), // Add spacing between the buttons
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle the "ยกเลิก" button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFed6a5a), // Set button color here
+                      ),
+                      child: Text('Cancel'),
                     ),
                   ],
                 ),
