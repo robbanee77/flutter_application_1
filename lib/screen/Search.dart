806b9758f97049773dc3589cacdaffb455fa748e
robbanee77 <robbanaa4959@gmail.com>
2023-08-08 23:01:01 +0700

    oik

--- lib/screen/Search.dart
+++ lib/screen/Search.dart
@@ -57,89 +57,80 @@ class _SearchState extends State<Search> {
                 context,
                 MaterialPageRoute(builder: (context) => historystu()),
               );
-              // ดักเหตุการณ์เมื่อกดปุ่ม Info ที่อยู่ฝั่งซ้ายของ AppBar
-              // คุณสามารถใส่โค้ดที่คุณต้องการทำเมื่อกดปุ่มนี้ได้ที่นี่
             },
           ),
         ],
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            children: [
-              SizedBox(height: 150.0), // ปรับความสูงได้ตามต้องการ
-              Image.asset(
-                "assets/images/search3.png",
-                width: 150,
-                height: 150,
-              ),
-              SizedBox(height: 0.0), // ปรับความสูงได้ตามต้องการ
-              Text(
-                "CODE OF SUBJECTS",
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF5ca4a9),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                SizedBox(height: 150.0),
+                Image.asset(
+                  "assets/images/search3.png",
+                  width: 150,
+                  height: 150,
                 ),
-              ),
-              SizedBox(height: 0.0), // ปรับความสูงได้ตามต้องการ
-              Text(
-                "( Ex: GE2100-101 )",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFed6a5a),
-                ),
-              ),
-              SizedBox(height: 25.0), // ปรับความสูงได้ตามต้องการ
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
+                SizedBox(height: 0.0),
+                Text(
+                  "CODE OF SUBJECTS",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
                     color: Color(0xFF5ca4a9),
-                    width: 1.0,
                   ),
-                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _searchController,
-                          decoration: InputDecoration(
-                            hintText: 'Search',
-                            border: InputBorder.none,
-                            prefixIcon: Icon(
-                              Icons.search,
-                              color: Color(0xFF5ca4a9),
+                SizedBox(height: 0.0),
+                Text(
+                  "( Ex: GE2100-101 )",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFed6a5a),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xFF5ca4a9),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _searchController,
+                            decoration: InputDecoration(
+                              hintText: 'Search',
+                              border: InputBorder.none,
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Color(0xFF5ca4a9),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          String searchTerm = _searchController.text;
-                          // Perform search logic here
-                          // You can use the entered search term to filter your data or perform any search operation
-
-                          // For demonstration purposes, let's print the search term
-
-                          // ดำเนินการค้นหาตรรกะที่นี่
-                          // คุณสามารถใช้ข้อความค้นหาที่ป้อนเพื่อกรองข้อมูลของคุณหรือดำเนินการค้นหาใดๆ ก็ได้
-
-                          // เพื่อการสาธิต ลองพิมพ์คำค้นหา
-                          print('Search Term: $searchTerm');
-                        },
-                        icon: Icon(Icons.send),
-                        color: Color(0xFFed6a5a),
-                      ),
-                    ],
+                        IconButton(
+                          onPressed: () {
+                            String searchTerm = _searchController.text;
+                            print('Search Term: $searchTerm');
+                          },
+                          icon: Icon(Icons.send),
+                          color: Color(0xFFed6a5a),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/screen/histostu.dart
+++ lib/screen/histostu.dart
@@ -29,6 +29,306 @@ class _historystuState extends State<historystu> {
         ),
         backgroundColor: Color(0xFF5ca4a9),
       ),
+      body: SingleChildScrollView(
+        // ใช้ SingleChildScrollView แทน Column เพื่อให้สามารถเลื่อนเนื้อหาได้
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  EdgeInsets.only(top: 45, left: 20, right: 20, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'History',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF5ca4a9),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(width: 10), // กำหนดระยะห่างระหว่างข้อความกับรูปภาพ
+                  Image.asset(
+                    'assets/images/histo2.png', // ตำแหน่งของไฟล์รูปภาพที่คุณต้องการใช้
+                    width: 30, // ปรับขนาดรูปภาพตามที่คุณต้องการ
+                    height: 30,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+              color: Colors.white, // สีของเส้น (ปรับตามที่คุณต้องการ)
+              margin: EdgeInsets.symmetric(horizontal: 20),
+            ),
+            Container(
+              padding:
+                  EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF5ca4a9),
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '08|08|2023',
+                      style: TextStyle(
+                        fontSize:
+                            20, // Adjust the font size for "There is" here
+                        color: Color(0xFFed6a5a),
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          ' The Faculty of Science and Technology has accepted your request in the E-business course.',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Container(
+              height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+              color: Colors.white, // สีของเส้น (ปรับตามที่คุณต้องการ)
+              margin: EdgeInsets.symmetric(horizontal: 20),
+            ),
+            Container(
+              padding:
+                  EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF5ca4a9),
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '07|08|2023',
+                      style: TextStyle(
+                        fontSize:
+                            20, // Adjust the font size for "There is" here
+                        color: Color(0xFFed6a5a),
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          '  You have submitted a request to open an E-business course.',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Container(
+              height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+              color: Colors.white, // สีของเส้น (ปรับตามที่คุณต้องการ)
+              margin: EdgeInsets.symmetric(horizontal: 20),
+            ),
+            Container(
+              padding:
+                  EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF5ca4a9),
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '07|08|2023',
+                      style: TextStyle(
+                        fontSize:
+                            20, // Adjust the font size for "There is" here
+                        color: Color(0xFFed6a5a),
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          '  You have submitted a request to open an Programming course.',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Container(
+              height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+              color: Colors.white, // สีของเส้น (ปรับตามที่คุณต้องการ)
+              margin: EdgeInsets.symmetric(horizontal: 20),
+            ),
+            // Container(
+            //   padding:
+            //       EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+            //   child: RichText(
+            //     text: TextSpan(
+            //       style: TextStyle(
+            //         fontSize: 15,
+            //         fontWeight: FontWeight.bold,
+            //         color: Color(0xFF5ca4a9),
+            //       ),
+            //       children: <TextSpan>[
+            //         TextSpan(
+            //           text: 'Faculty of Science and Technology',
+            //           style: TextStyle(
+            //             fontSize:
+            //                 20, // Adjust the font size for "There is" here
+            //             color: Color(0xFFed6a5a),
+            //           ),
+            //         ),
+            //         TextSpan(
+            //           text:
+            //               ' Computer Programming course already reserved couse to you.',
+            //         ),
+            //       ],
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // Container(
+            //   height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+            //   color: Colors.white, // สีของเส้น (ปรับตามที่คุณต้องการ)
+            //   margin: EdgeInsets.symmetric(horizontal: 20),
+            // ),
+            // Container(
+            //   padding:
+            //       EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+            //   child: RichText(
+            //     text: TextSpan(
+            //       style: TextStyle(
+            //         fontSize: 15,
+            //         fontWeight: FontWeight.bold,
+            //         color: Color(0xFF5ca4a9),
+            //       ),
+            //       children: <TextSpan>[
+            //         TextSpan(
+            //           text: '611437710',
+            //           style: TextStyle(
+            //             fontSize:
+            //                 20, // Adjust the font size for "There is" here
+            //             color: Color(0xFFed6a5a),
+            //           ),
+            //         ),
+            //         TextSpan(
+            //           text:
+            //               ' There is a request for reservation of the Network I course.',
+            //         ),
+            //       ],
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // Container(
+            //   height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+            //   color: Color(0xFFed6a5a), // สีของเส้น (ปรับตามที่คุณต้องการ)
+            //   margin: EdgeInsets.symmetric(horizontal: 20),
+            // ),
+            // Container(
+            //   padding:
+            //       EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+            //   child: RichText(
+            //     text: TextSpan(
+            //       style: TextStyle(
+            //         fontSize: 15,
+            //         fontWeight: FontWeight.bold,
+            //         color: Color(0xFF5ca4a9),
+            //       ),
+            //       children: <TextSpan>[
+            //         TextSpan(
+            //           text: '631431453',
+            //           style: TextStyle(
+            //             fontSize:
+            //                 20, // Adjust the font size for "There is" here
+            //             color: Color(0xFFed6a5a),
+            //           ),
+            //         ),
+            //         TextSpan(
+            //           text:
+            //               ' There is a request for reservation of the Computer Platform Technology course.',
+            //         ),
+            //       ],
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // Container(
+            //   height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+            //   color: Color(0xFFed6a5a), // สีของเส้น (ปรับตามที่คุณต้องการ)
+            //   margin: EdgeInsets.symmetric(horizontal: 20),
+            // ),
+            // Container(
+            //   padding:
+            //       EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+            //   child: RichText(
+            //     text: TextSpan(
+            //       style: TextStyle(
+            //         fontSize: 15,
+            //         fontWeight: FontWeight.bold,
+            //         color: Color(0xFF5ca4a9),
+            //       ),
+            //       children: <TextSpan>[
+            //         TextSpan(
+            //           text: '631434533',
+            //           style: TextStyle(
+            //             fontSize:
+            //                 20, // Adjust the font size for "There is" here
+            //             color: Color(0xFFed6a5a),
+            //           ),
+            //         ),
+            //         TextSpan(
+            //           text:
+            //               ' There is a request for reservation of the Software Development Process II course.',
+            //         ),
+            //       ],
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // Container(
+            //   height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+            //   color: Color(0xFFed6a5a), // สีของเส้น (ปรับตามที่คุณต้องการ)
+            //   margin: EdgeInsets.symmetric(horizontal: 20),
+            // ),
+            // Container(
+            //   padding:
+            //       EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
+            //   child: RichText(
+            //     text: TextSpan(
+            //       style: TextStyle(
+            //         fontSize: 15,
+            //         fontWeight: FontWeight.bold,
+            //         color: Color(0xFF5ca4a9),
+            //       ),
+            //       children: <TextSpan>[
+            //         TextSpan(
+            //           text: '621433421',
+            //           style: TextStyle(
+            //             fontSize:
+            //                 20, // Adjust the font size for "There is" here
+            //             color: Color(0xFFed6a5a),
+            //           ),
+            //         ),
+            //         TextSpan(
+            //           text:
+            //               ' There is a request for reservation of the Social Issues and Ethics for IT Professional course.',
+            //         ),
+            //       ],
+            //     ),
+            //     textAlign: TextAlign.left,
+            //   ),
+            // ),
+            // Container(
+            //   height: 2, // ความสูงของเส้น (ปรับตามที่คุณต้องการ)
+            //   color: Color(0xFFed6a5a), // สีของเส้น (ปรับตามที่คุณต้องการ)
+            //   margin: EdgeInsets.symmetric(horizontal: 20),
+            // ),
+          ],
+        ),
+      ),
     );
   }
 }
