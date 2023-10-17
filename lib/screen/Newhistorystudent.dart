9366244041d846386c2c5dd67917e60dd4568f2f
robbanee77 <robbanaa4959@gmail.com>
2023-10-17 14:39:04 +0700

    We are already 2 month

--- lib/screen/Newhistorystudent.dart
+++ lib/screen/Newhistorystudent.dart
@@ -1,3 +1,174 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter_application_1/screen/LogoutPage.dart';
+// import 'package:flutter_application_1/screen/Newnotifstudent.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: NewHistoryStudent(),
+//     );
+//   }
+// }
+
+// class NewHistoryStudent extends StatefulWidget {
+//   const NewHistoryStudent({Key? key}) : super(key: key);
+
+//   @override
+//   _NewHistoryStudentState createState() => _NewHistoryStudentState();
+// }
+
+// class _NewHistoryStudentState extends State<NewHistoryStudent> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFFe6ebe0),
+//       appBar: AppBar(
+//         title: Center(
+//           // ทำให้ข้อความอยู่ตรงกลางใน AppBar
+//           child: Row(
+//             children: [
+//               Image.asset(
+//                 'assets/images/pro.png',
+//                 width: 30,
+//                 height: 30,
+//                 fit: BoxFit.contain,
+//               ),
+//               SizedBox(width: 8),
+//               Text('History'), // ข้อความใน AppBar
+//             ],
+//           ),
+//         ),
+//         backgroundColor: Color(0xFF5ca4a9),
+//         actions: [
+//           IconButton(
+//             icon: Image.asset(
+//               'assets/images/noti1.png',
+//               width: 30,
+//               height: 30,
+//               fit: BoxFit.contain,
+//             ),
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => NewNotifstudent()),
+//               );
+//             },
+//           ),
+//           IconButton(
+//             icon: Image.asset(
+//               'assets/images/logout.png',
+//               width: 30,
+//               height: 30,
+//               fit: BoxFit.contain,
+//             ),
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => LogoutPage()),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//       body: Center(
+//         // ทำให้ข้อความอยู่ตรงกลางใน body
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Padding(
+//               padding: const EdgeInsets.all(16.0),
+//               child: Text(
+//                 'HISTORY',
+//                 textAlign: TextAlign.center, // ทำให้อยู่ตรงกลาง
+//                 style: TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold,
+//                   color: Color(0xFF5ca4a9),
+//                 ),
+//               ),
+//             ),
+//             Expanded(
+//               child: StreamBuilder<QuerySnapshot>(
+//                 stream: FirebaseFirestore.instance
+//                     .collection('booking')
+//                     .snapshots(),
+//                 builder: (context, snapshot) {
+//                   if (snapshot.hasError) {
+//                     return Center(
+//                         child: Text('เกิดข้อผิดพลาด: ${snapshot.error}'));
+//                   }
+
+//                   if (snapshot.connectionState == ConnectionState.waiting) {
+//                     return Center(child: CircularProgressIndicator());
+//                   }
+
+//                   final bookingDocs = snapshot.data?.docs;
+
+//                   if (bookingDocs == null || bookingDocs.isEmpty) {
+//                     return Center(
+//                         child: Text(
+//                       'No reservations.',
+//                       style: TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.bold,
+//                           color: Color(0xFF5ca4a9)),
+//                     ));
+//                   }
+
+//                   return ListView.builder(
+//                     itemCount: bookingDocs.length,
+//                     itemBuilder: (context, index) {
+//                       final bookingData =
+//                           bookingDocs[index].data() as Map<String, dynamic>;
+
+//                       final code = bookingData['data']['Code'] as String;
+//                       final program = bookingData['data']['Program'] as String;
+
+//                       return Card(
+//                         elevation: 5,
+//                         margin:
+//                             EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+//                         child: ListTile(
+//                           title: Text(
+//                             'You have a course reservation.',
+//                             style: TextStyle(
+//                                 fontSize: 18.0,
+//                                 fontWeight: FontWeight.bold,
+//                                 color: Color(0xFF5ca4a9)),
+//                           ),
+//                           subtitle: Column(
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               Text('Code: $code'),
+//                               Text('Program: $program'),
+//                             ],
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -30,13 +201,31 @@ class NewHistoryStudent extends StatefulWidget {
 }
 
 class _NewHistoryStudentState extends State<NewHistoryStudent> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>?>
+      getUserData() async {
+    final user = _auth.currentUser;
+    if (user != null) {
+      final userDoc = await _firestore
+          .collection('booking')
+          .where('email', isEqualTo: user.email)
+          .get();
+      final bookingDocs = userDoc.docs;
+      if (bookingDocs.isNotEmpty) {
+        return bookingDocs;
+      }
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFe6ebe0),
       appBar: AppBar(
         title: Center(
-          // ทำให้ข้อความอยู่ตรงกลางใน AppBar
           child: Row(
             children: [
               Image.asset(
@@ -46,7 +235,7 @@ class _NewHistoryStudentState extends State<NewHistoryStudent> {
                 fit: BoxFit.contain,
               ),
               SizedBox(width: 8),
-              Text('History'), // ข้อความใน AppBar
+              Text('History'),
             ],
           ),
         ),
@@ -83,7 +272,6 @@ class _NewHistoryStudentState extends State<NewHistoryStudent> {
         ],
       ),
       body: Center(
-        // ทำให้ข้อความอยู่ตรงกลางใน body
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -91,7 +279,7 @@ class _NewHistoryStudentState extends State<NewHistoryStudent> {
               padding: const EdgeInsets.all(16.0),
               child: Text(
                 'HISTORY',
-                textAlign: TextAlign.center, // ทำให้อยู่ตรงกลาง
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
@@ -100,39 +288,34 @@ class _NewHistoryStudentState extends State<NewHistoryStudent> {
               ),
             ),
             Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('booking')
-                    .snapshots(),
+              child: FutureBuilder(
+                future: getUserData(),
                 builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Center(
-                        child: Text('เกิดข้อผิดพลาด: ${snapshot.error}'));
-                  }
-
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Center(child: CircularProgressIndicator());
-                  }
-
-                  final bookingDocs = snapshot.data?.docs;
-
-                  if (bookingDocs == null || bookingDocs.isEmpty) {
+                  } else if (snapshot.hasError) {
                     return Center(
-                        child: Text(
-                      'No reservations.',
-                      style: TextStyle(
+                        child: Text('เกิดข้อผิดพลาด: ${snapshot.error}'));
+                  } else if (snapshot.data == null) {
+                    return Center(
+                      child: Text(
+                        'No reservations.',
+                        style: TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.bold,
-                          color: Color(0xFF5ca4a9)),
-                    ));
+                          color: Color(0xFF5ca4a9),
+                        ),
+                      ),
+                    );
                   }
 
+                  final bookingDocs =
+                      snapshot.data as List<QueryDocumentSnapshot>;
                   return ListView.builder(
                     itemCount: bookingDocs.length,
                     itemBuilder: (context, index) {
                       final bookingData =
                           bookingDocs[index].data() as Map<String, dynamic>;
-
                       final code = bookingData['data']['Code'] as String;
                       final program = bookingData['data']['Program'] as String;
 
@@ -144,9 +327,10 @@ class _NewHistoryStudentState extends State<NewHistoryStudent> {
                           title: Text(
                             'You have a course reservation.',
                             style: TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF5ca4a9)),
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF5ca4a9),
+                            ),
                           ),
                           subtitle: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
--- lib/screen/Newnotifstudent.dart
+++ lib/screen/Newnotifstudent.dart
@@ -1,14 +1,177 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter_application_1/screen/LogoutPage.dart';
+// import 'package:flutter_application_1/screen/Newhistorystudent.dart';
+// import 'package:flutter_application_1/screen/histostu.dart';
+
+// class NewNotifstudent extends StatelessWidget {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFFe6ebe0),
+//       appBar: AppBar(
+//         title: Row(
+//           children: [
+//             Image.asset(
+//               'assets/images/pro.png',
+//               width: 30,
+//               height: 30,
+//               fit: BoxFit.contain,
+//             ),
+//             SizedBox(width: 8),
+//             Text('Notifications'),
+//           ],
+//         ),
+//         backgroundColor: Color(0xFF5ca4a9),
+//         actions: [
+//           IconButton(
+//             icon: Image.asset(
+//               'assets/images/histo.png',
+//               width: 30,
+//               height: 30,
+//               fit: BoxFit.contain,
+//             ),
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => NewHistoryStudent()),
+//               );
+//             },
+//           ),
+//           IconButton(
+//             icon: Image.asset(
+//               'assets/images/logout.png',
+//               width: 30,
+//               height: 30,
+//               fit: BoxFit.contain,
+//             ),
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => LogoutPage()),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//       body: Column(
+//         children: [
+//           Padding(
+//             padding: EdgeInsets.all(20),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Text(
+//                   'Notifications',
+//                   style: TextStyle(
+//                     fontSize: 35,
+//                     fontWeight: FontWeight.bold,
+//                     color: Color(0xFF5ca4a9),
+//                   ),
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 SizedBox(width: 10), // กำหนดระยะห่างระหว่างข้อความกับรูปภาพ
+//                 Image.asset(
+//                   'assets/images/noti4.png', // ตำแหน่งของไฟล์รูปภาพที่คุณต้องการใช้
+//                   width: 30, // ปรับขนาดรูปภาพตามที่คุณต้องการ
+//                   height: 30,
+//                 ),
+//               ],
+//             ),
+//           ),
+//           Expanded(
+//             child: StreamBuilder<QuerySnapshot>(
+//               stream:
+//                   FirebaseFirestore.instance.collection('confirm').snapshots(),
+//               builder: (context, confirmSnapshot) {
+//                 if (confirmSnapshot.connectionState ==
+//                     ConnectionState.waiting) {
+//                   return CircularProgressIndicator();
+//                 }
+
+//                 final confirmDocuments = confirmSnapshot.data!.docs;
+
+//                 return StreamBuilder<QuerySnapshot>(
+//                   stream: FirebaseFirestore.instance
+//                       .collection('cancel')
+//                       .snapshots(),
+//                   builder: (context, cancelSnapshot) {
+//                     if (cancelSnapshot.connectionState ==
+//                         ConnectionState.waiting) {
+//                       return CircularProgressIndicator();
+//                     }
+
+//                     final cancelDocuments = cancelSnapshot.data!.docs;
+
+//                     // Check if both collections are empty
+//                     if (confirmDocuments.isEmpty && cancelDocuments.isEmpty) {
+//                       return Center(
+//                         child: Text(
+//                           "No notifications",
+//                           style: TextStyle(
+//                               fontSize: 18.0,
+//                               fontWeight: FontWeight.bold,
+//                               color: Color(0xFF5ca4a9)),
+//                         ),
+//                       );
+//                     }
+
+//                     return ListView.builder(
+//                       itemCount:
+//                           confirmDocuments.length + cancelDocuments.length,
+//                       itemBuilder: (context, index) {
+//                         if (index < confirmDocuments.length) {
+//                           return _buildDocumentBlock(
+//                               'Your information has been verified.');
+//                         } else {
+//                           return _buildDocumentBlock(
+//                               'Your information has been canceled.');
+//                         }
+//                       },
+//                     );
+//                   },
+//                 );
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildDocumentBlock(String text) {
+//     return Container(
+//       padding: EdgeInsets.all(16),
+//       margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+//       decoration: BoxDecoration(
+//         color: Colors.white,
+//         borderRadius: BorderRadius.circular(10),
+//         boxShadow: [
+//           BoxShadow(
+//             color: Colors.grey.withOpacity(0.5),
+//             spreadRadius: 5,
+//             blurRadius: 7,
+//             offset: Offset(0, 3),
+//           ),
+//         ],
+//       ),
+//       child: Text(text),
+//     );
+//   }
+// }
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_application_1/screen/LogoutPage.dart';
-import 'package:flutter_application_1/screen/Newhistorystudent.dart';
-import 'package:flutter_application_1/screen/histostu.dart';
 
 class NewNotifstudent extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,118 +190,113 @@ class NewNotifstudent extends StatelessWidget {
           ],
         ),
         backgroundColor: Color(0xFF5ca4a9),
-        actions: [
-          IconButton(
-            icon: Image.asset(
-              'assets/images/histo.png',
-              width: 30,
-              height: 30,
-              fit: BoxFit.contain,
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NewHistoryStudent()),
-              );
-            },
-          ),
-          IconButton(
-            icon: Image.asset(
-              'assets/images/logout.png',
-              width: 30,
-              height: 30,
-              fit: BoxFit.contain,
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LogoutPage()),
-              );
-            },
-          ),
-        ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Notifications',
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF5ca4a9),
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(width: 10), // กำหนดระยะห่างระหว่างข้อความกับรูปภาพ
-                Image.asset(
-                  'assets/images/noti4.png', // ตำแหน่งของไฟล์รูปภาพที่คุณต้องการใช้
-                  width: 30, // ปรับขนาดรูปภาพตามที่คุณต้องการ
-                  height: 30,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream:
-                  FirebaseFirestore.instance.collection('confirm').snapshots(),
-              builder: (context, confirmSnapshot) {
-                if (confirmSnapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
+      body: FutureBuilder<User?>(
+        future: _auth.authStateChanges().first,
+        builder: (context, userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
 
-                final confirmDocuments = confirmSnapshot.data!.docs;
+          if (!userSnapshot.hasData) {
+            // User is not logged in, handle this case as needed
+            return Center(
+              child: Text("Please log in to view notifications."),
+            );
+          }
 
-                return StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance
-                      .collection('cancel')
-                      .snapshots(),
-                  builder: (context, cancelSnapshot) {
-                    if (cancelSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    }
+          final User? user = userSnapshot.data;
+          final String userEmail = user?.email ?? ""; // Get user's email
 
-                    final cancelDocuments = cancelSnapshot.data!.docs;
+          return StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('confirm')
+                .where('email', isEqualTo: userEmail)
+                .snapshots(),
+            builder: (context, confirmSnapshot) {
+              if (confirmSnapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
 
-                    // Check if both collections are empty
-                    if (confirmDocuments.isEmpty && cancelDocuments.isEmpty) {
-                      return Center(
-                        child: Text(
-                          "No notifications",
-                          style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF5ca4a9)),
-                        ),
-                      );
-                    }
+              final confirmDocuments = confirmSnapshot.data!.docs;
 
-                    return ListView.builder(
-                      itemCount:
-                          confirmDocuments.length + cancelDocuments.length,
-                      itemBuilder: (context, index) {
-                        if (index < confirmDocuments.length) {
-                          return _buildDocumentBlock(
-                              'Your information has been verified.');
-                        } else {
-                          return _buildDocumentBlock(
-                              'Your information has been canceled.');
-                        }
-                      },
+              return StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('cancel')
+                    .where('email', isEqualTo: userEmail)
+                    .snapshots(),
+                builder: (context, cancelSnapshot) {
+                  if (cancelSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+
+                  final cancelDocuments = cancelSnapshot.data!.docs;
+
+                  if (confirmDocuments.isEmpty && cancelDocuments.isEmpty) {
+                    return Center(
+                      child: Text(
+                        "No notifications",
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF5ca4a9)),
+                      ),
                     );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
+                  }
+
+                  return ListView.builder(
+                    itemCount: confirmDocuments.length + cancelDocuments.length,
+                    itemBuilder: (context, index) {
+                      if (index < confirmDocuments.length) {
+                        return _buildDocumentBlock(
+                            'Your information has been verified.');
+                      } else {
+                        return _buildDocumentBlock(
+                            'Your information has been canceled.');
+                      }
+                    },
+                  );
+                },
+              );
+
+              return StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('cancel') // Change to 'cancel' collection
+                    .where('email', isEqualTo: userEmail)
+                    .snapshots(),
+                builder: (context, cancelSnapshot) {
+                  if (cancelSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+
+                  final cancelDocuments = cancelSnapshot.data!.docs;
+
+                  if (cancelDocuments.isEmpty) {
+                    return Center(
+                      child: Text(
+                        "No cancel notifications",
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF5ca4a9)),
+                      ),
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemCount: cancelDocuments.length,
+                    itemBuilder: (context, index) {
+                      return _buildDocumentBlock(
+                          'Your information has been canceled.');
+                    },
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
     );
   }
